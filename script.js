4c68bb9dc34046aa5dad36a0ffe8733e0139236f
unknown <ankitsharma101299@gmail.com>
2020-04-27 12:07:03 +0530

    Added script file for validating inputs

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = document.getElementById("first_name");
+let lastName = document.getElementById("last_name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+let elArr = [firstName,lastName,email,password];
